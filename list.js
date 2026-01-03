c34e8b0299a7b8b9a64efba92ec13ed9fcc6ec1d
Seamus McMichael <40214975+mcmichaelseamus@users.noreply.github.com>
2026-01-03 03:15:39 +0000

    Update list.js

--- list.js
+++ list.js
@@ -1,6 +1,6 @@
-    var todos = [''];
+    let todo_items = [''];
 
-var input = prompt("What would you like to do?");
+let input = prompt("What would you like to do?");
 
 while(input !== "quit")   {
     if(input === "list") {
